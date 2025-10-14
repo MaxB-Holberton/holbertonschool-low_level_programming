ba27dbc43f0121daafe580e230e31dbf92f28cd1
MaxB-Holberton <12107@holbertonstudents.com>
2025-10-14 23:21:51 +0000

    This is a new commit

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+/**
+ * print_alphabet - Print the Alphabet
+ * @void - does nothing
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		_putchar(i);
+	_putchar(10);
+}
---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * The Main Description
+ */
+
+/**
+ * main - the main function
+ * @void - does nothing 
+ *
+ * Return: 0
+ */
+int main(void)
+{
+        print_alphabet();
+        return (0);
+}
+
