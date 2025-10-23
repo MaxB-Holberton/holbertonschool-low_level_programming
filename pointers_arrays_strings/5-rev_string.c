ac6164dca45bb8e9dde822070dde1056e936dd1f
MaxB-Holberton <12107@holbertonstudents.com>
2025-10-23 00:32:46 +0000

    new commit

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,28 +1,26 @@
 #include "main.h"
 /**
- * rev_string - reverse the string 
+ * rev_string - reverse the string
  * @s: the string to write
  *
  * Return: void
  */
 void rev_string(char *s)
 {
-	char *start = s;
-	char *end = s;
+	char *startofstr = s;
+	char *endofstr = s;
 	char buffer;
 
-	while (*end != '\0')
-		end++;
-
-	while (start < end)
+	while (*endofstr != '\0')
 	{
-		buffer = *start;
-		*start = *end;
-		*s = *start;
-		*end = buffer;
-		
-		s++;
-		start++;
-		end--;
+		endofstr++;
 	}
+	while (startofstr < endofstr)
+	{
+		endofstr--;
+		buffer = *endofstr;
+		*endofstr = *startofstr;
+		*startofstr = buffer;
+		startofstr++;
+	} 
 }
