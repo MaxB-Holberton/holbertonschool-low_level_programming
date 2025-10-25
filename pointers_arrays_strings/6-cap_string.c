01d3cc89faff6054866572c9da235db0d56b3cb3
MaxB-Holberton <12107@holbertonstudents.com>
2025-10-25 12:42:01 +0000

    debugging

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,7 +6,6 @@
  */
 void check_letter(char *s)
 {
-	s++;
 	if (*s > 96 && *s < 123)
 		*s = *s - 32;
 }
@@ -19,22 +18,29 @@ void check_letter(char *s)
 char *cap_string(char *s)
 {
 	char *cpy = s;
-	int i = 0;
 
+	check_letter(s);
 	while (*s != '\0')
 	{
 		if  (*s == ' ' || *s == ',' || *s == ';' || *s == '.')
-			check_letter(s);
-		else if (*s == '!' || *s == '?' || *s == '"' || *s == '(' || *s == ')')
-			check_letter(s);
-		else if (*s == '\n' || *s == '\t')
-			check_letter(s);
-		else if (*s == '{' || *s == '}')
-			check_letter(s);
-		else if (i == 0)
 		{
+			s++;
 			check_letter(s);
-			i++;
+		}
+		else if (*s == '!' || *s == '?' || *s == '"' || *s == '(' || *s == ')')
+		{
+			s++;
+			check_letter(s);
+		}
+		else if (*s == '\n' || *s == '\t')
+		{
+			s++;
+			check_letter(s);
+		}
+		else if (*s == '{' || *s == '}')
+		{
+			s++;
+			check_letter(s);
 		}
 		s++;
 	}
