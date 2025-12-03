0ad6c1fd8b5c880396f3207af6ca12ba01513fa0
MaxB-Holberton <12107@holbertonstudents.com>
2025-12-04 09:46:38 +1100

    added task 6 - delete hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_delet - delete the hash table
+ * @ht: the hash table to use
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	hash_node_t *next_node;
+	unsigned long int size = ht->size;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		current = ht->array[i];
+		if (current == NULL)
+			continue;
+
+		while (current != NULL)
+		{
+			next_node = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = NULL;
+			current = next_node;
+		}
+	}
+}
